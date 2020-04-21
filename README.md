e829a74503f65f5bc059ad8ccf95c2e3cac24f53
AlanCienega <craulalan115@gmail.com>
2020-04-21 11:01:45 -0500

    loop con fecha de GLib

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # gnome-test-extension
+## Descarga e instala
+git clone https://github.com/AlanCienega/gnome-test-extension.git
+mv /gnome-test-extension ~/.local/share/gnome-shell/extensions/alan@cienegaserrano.com
--- extension.js
+++ extension.js
@@ -1,7 +1,47 @@
 const St = imports.gi.St; //importamos St para tener elementos de interfaz de usuario
 const Main = imports.ui.main; //importamos main para traer todos los elementos de UI (panel)
+const Mainloop = imports.mainloop;
+const GLib = imports.gi.GLib;
 
-let panelButton, panelButtonText;
+let panelButton, panelButtonText, timeout;
+
+function setButtonText(){
+
+    var arr =[]
+
+    /*
+    //agregar fecha del sistema
+    var [ok, out, err, exit] = GLib.spawn_command_line_sync("date") 
+    arr.push(out.toString().replace('\n', ''))
+    */
+    /*
+    // Ejecutamos pgrep gedit para revisar si gedit se esta ejecutando
+    var [ok, out, err, exit] = GLib.spawn_command_line_sync("pgrep gedit") 
+        if (out>0) {
+            arr.push('Gedit')
+        }
+    */
+    /*
+    // Private
+    var [ok, out, err, exit] = GLib.spawn_command_line_sync('/bin/bas -c "ifconfig -a | grep tun0') 
+        if (out>0) {
+            arr.push('private')
+        }
+    */
+    /*
+    //fecha con javascript
+    var date = new Date();
+    arr.push(date)
+    */
+    // fecha con  GLib, recomendada para poder personalizar
+    var now = GLib.DateTime.new_now_local();
+    var str = now.format("%d-%m-%Y %H:%M:%S");
+    arr.push(str);
+
+    //Mostrar los comandos
+    panelButtonText.set_text(arr.join('  '))
+    return true;
+}
 
 function init(){
     panelButton = new St.Bin({
@@ -18,6 +58,7 @@ function init(){
 
 function enable(){
     Main.panel._rightBox.insert_child_at_index(panelButton,1)
+    timeout = Mainloop.timeout_add_seconds(1.0, setButtonText)
 }
 
 function disable(){
